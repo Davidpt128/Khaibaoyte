149b01b694a055820015d012b2371430c0c5790e
Đạt Phạm Thành <thanhdat.12081998@gmail.com>
2022-03-22 17:28:24 +0700

    khaibaoyte

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
-// if(document.querySelector('#signup-form .actions ul li:last-child a').onload == 'Finish'){
-//     alert('456');
-// }
-document.querySelector('#signup-form .actions ul li:last-child a').onload = function() {
-    alert('123');
+document.querySelector('#signup-form .actions ul li:last-child a').onclick = () => {
+    //input
+    let arrInput = document.querySelectorAll('#signup-form input, #signup-form select');
+    console.log('arrInput', arrInput);
+    let thongTin = {};
+    for (let input of arrInput){
+        let {id, value} = input;
+        console.log(id,value);
+        thongTin = {...thongTin, [id]:value};
+    }
+    console.log('thông tin', thongTin);
+
+    let contentHTML = '';
+    for (let key in thongTin) {
+        contentHTML += `
+            <tr>
+                <td>${key}</td>
+                <td>${thongTin[key]}</td>
+            </tr>
+        `
+    }
+
+    document.querySelector('#showInfo').innerHTML = contentHTML;
 }
+
+    
