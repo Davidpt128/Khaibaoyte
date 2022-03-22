2ad1f8dc53ce7f0fed77efc9550c10bbb6b8ed6f
Đạt Phạm Thành <thanhdat.12081998@gmail.com>
2022-03-22 16:07:53 +0700

    khaibaoyte1

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// if(document.querySelector('#signup-form .actions ul li:last-child a').onload == 'Finish'){
+//     alert('456');
+// }
+document.querySelector('#signup-form .actions ul li:last-child a').onload = function() {
+    alert('123');
+}
